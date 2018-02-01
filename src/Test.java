67837cac0bceb0f93ab0a1869fcb019c6874637f
kaspe <kaspe@DESKTOP-GR0OHS1>
2018-02-01 20:00:31 +0100

    h

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Björn äger");
-		System.out.println("Kasper äger med!");
+		System.out.println("Kasper äger med!!");
 		
 	}
 
